0db006f693c4daab7e3ea09c06effc504dc3b062
rfoltyns <r.foltynski@gmail.com>
2021-01-29 18:48:10 +0000

    PatternLayout - wrong tool for the job

--- log4j2-elasticsearch-jest-springboot/src/main/java/com/github/rfoltyns/Application.java
+++ log4j2-elasticsearch-jest-springboot/src/main/java/com/github/rfoltyns/Application.java
@@ -1,5 +1,7 @@
 package com.github.rfoltyns;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -7,23 +9,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws JsonProcessingException {
 
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
         Logger logger = LogManager.getLogger("elasticsearch");
-        logger.info("Hello, World!");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        DataToSend data = new DataToSend("Hello, World!");
+        String json = objectMapper.writeValueAsString(data);
+
+        logger.info(json);
 
         LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));
 
         applicationContext.close();
         LoggingSystem.get(logger.getClass().getClassLoader()).getShutdownHandler().run();
+    }
+
+    private static class DataToSend {
+
+        final long timestamp = System.currentTimeMillis();
+        final String message;
+
+        private DataToSend(String message) {
+            this.message = message;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
     }
 
 }
