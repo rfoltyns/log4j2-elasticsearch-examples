0d5157462f22cfd6dd0c9b68e925c91111682022
rfoltyns <r.foltynski@gmail.com>
2021-04-07 21:23:53 +0100

    Add more examples
    * Minimal XML for 6.x
    * Minimal XML for 7.x
    * Virtual Properties and ThreadContext
    * Composable IndexTemplate
    * ILMPolicy

--- log4j2-elasticsearch-jest-springboot/src/main/java/com/github/rfoltyns/Application.java
+++ log4j2-elasticsearch-jest-springboot/src/main/java/com/github/rfoltyns/Application.java
@@ -2,11 +2,13 @@ package com.github.rfoltyns;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
@@ -18,12 +20,16 @@ public class Application {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
         Logger logger = LogManager.getLogger("elasticsearch");
+
+        ThreadContext.put("myFavouriteVariable", UUID.randomUUID().toString());
         logger.info("Hello, World!");
+        ThreadContext.remove("myFavouriteVariable");
 
         LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(5));
 
         applicationContext.close();
         LoggingSystem.get(logger.getClass().getClassLoader()).getShutdownHandler().run();
+
     }
 
 }
